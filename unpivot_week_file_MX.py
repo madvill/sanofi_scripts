0115b439c9c3b16a0213859002f2c714ad93b6fc
madvill <martin.adville@sfr.fr>
2022-04-07 09:55:47 +0200

    unpivot_week_file details

--- unpivot_week_file_MX.py
+++ unpivot_week_file_MX.py
@@ -2,12 +2,6 @@ from ntpath import join
 import os
 import sys
 import pandas as pd
-"""
-dir = str(input("Where is the directory ?"))
-file = str(input("What's the name of the xlsx file ?"))
-#dir='/content/'
-#file = 'IQVIA_Weekly_SO_MX_202212.xlsx'
-"""
 from pickle import FALSE
 import argparse
 from utils import set_log_level, get_logger
@@ -17,23 +11,20 @@ logger = get_logger(sys.argv[0])
 
 
 def main():
-    parser = argparse.ArgumentParser(description='lol')
+    parser = argparse.ArgumentParser()
     parser.add_argument('--file', help="Input file path", required=True)
     parser.add_argument('--verbose', help="Debug mode", required=False, default=False)
 
-
     args = parser.parse_args()
-
-    file = args.file
 
     if args.verbose:
         set_log_level(logger,"debug")
         logger.debug(f"Command line arguments: {args}")
     
-    df = pd.read_excel(file)
+    df = pd.read_excel(args.file)
 
     logger.info('File read successful')
-
+    #
     col_un = []
     col_lc = []
     other_col = []
@@ -49,9 +40,6 @@ def main():
     df.rename(inplace=True,columns=lambda x: str(x)[12:]+'_'+str(x)[9:11] if x in col_un else x)
 
     col_un = [a[12:]+'_'+a[9:11] for a in col_un]
-    #tmp_df = pd.melt(df, id_vars=other_col+col_lc, value_vars=col_un, var_name='WeekOfYear', value_name='Unit')
-
-    #new_df = pd.melt(tmp_df, id_vars=other_col+['WeekOfYear','Unit'], value_vars=col_lc, var_name='WeekOfYear1', value_name='Value').fillna(0)
 
     logger.info('Start meltings')
 
@@ -62,15 +50,10 @@ def main():
 
     new_df = pd.concat([tmp_df1,tmp_df2['Value']],axis=1,join='inner')
 
-    #del new_df['WeekOfYear1']
-    #logger.info('Start WeekOfYear treatment')
-    #new_df['WeekOfYear'] = [a[12:]+'_'+a[9:11] for a in new_df['WeekOfYear']]
-
     logger.info('Start writing in csv')
-    new_df.to_csv(file[:-5]+'_pivoted.csv',index=False)
+    new_df.to_csv(args.file[:-5]+'_pivoted.csv',index=False)
 
 
 if __name__ == "__main__":
     set_log_level(logger,"info") 
     main()
-
