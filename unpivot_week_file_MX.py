179e2cd799fd950c69450976e8e72cff546ce0cd
madvill <martin.adville@sfr.fr>
2022-04-07 17:51:26 +0200

    unpivoted fct validated for JP MX

--- unpivot_week_file_MX.py
+++ unpivot_week_file_MX.py
@@ -8,7 +8,7 @@ logger = get_logger(sys.argv[0])
 
 
 def main():
-    #Arguments of the function
+    # Arguments of the function
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", help="Input file path", required=True)
     parser.add_argument("--country", help="Origin country of the file", required=True)
@@ -20,14 +20,14 @@ def main():
         set_log_level(logger, "debug")
         logger.debug(f"Command line arguments: {args}")
 
-    #Read excel file
+    # Read excel file
     df = pd.read_excel(args.file)
 
     logger.info("File read successful")
-    
-    if args.country == 'MX':
 
-        #Make groups over column names (Un, Lc and others)
+    if args.country == "MX":
+
+        # Make groups over column names (Un, Lc and others)
         col_un = []
         col_lc = []
         other_col = []
@@ -40,7 +40,7 @@ def main():
                 other_col.append(i)
 
         logger.info("Start WeekOfYear treatment")
-        #Changing column name of date columns in YYYY_WW format
+        # Changing column name of date columns in YYYY_WW format
         df.rename(
             inplace=True,
             columns=lambda x: str(x)[12:] + "_" + str(x)[9:11] if x in col_un else x,
@@ -49,7 +49,7 @@ def main():
         col_un = [a[12:] + "_" + a[9:11] for a in col_un]
 
         logger.info("Start meltings")
-        #Create temporary dataframes with unpivoted Un and Lc
+        # Create temporary dataframes with unpivoted Un and Lc
         tmp_df1 = pd.melt(
             df,
             id_vars=other_col,
@@ -66,48 +66,52 @@ def main():
         )
 
         logger.info("Start concat")
-        #Concatenate the temporary dataframes to get the unpivot on only one dataframe
+        # Concatenate the temporary dataframes to get the unpivot on only one dataframe
         new_df = pd.concat([tmp_df1, tmp_df2["Value"]], axis=1, join="inner")
-    elif args.country == 'JP':
+    elif args.country == "JP":
         col_date = []
         other_col = []
         for i in df.columns:
-            if "/" in i :
+            if "/" in i:
                 col_date.append(i)
-            else :
+            else:
                 other_col.append(i)
-        
+
         logger.info("Start WeekOfYear treatment")
-        #Changing column name of date columns in YYYY_WW format
+        # Changing column name of date columns in YYYY_WW format
 
         df.rename(
             inplace=True,
-            columns=lambda x:str(x).replace('-','') if x in col_date else x,
+            columns=lambda x: str(x).replace("-", "") if x in col_date else x,
         )
 
-        col_date = [str(x).replace('-','') for x in col_date]
-        
+        col_date = [str(x).replace("-", "") for x in col_date]
+
         df.rename(
             inplace=True,
-            columns=lambda x: str(x)[0:4]+'0'+str(x)[4:5] if len(x)==6 and x in col_date else x,
+            columns=lambda x: str(x)[0:5] + "0" + str(x)[5:]
+            if len(x) == 6 and x in col_date
+            else x,
         )
 
-        col_date = [str(x)[0:4]+'0'+str(x)[4:5] if len(x)==6 else str(x) for x in col_date]
+        col_date = [
+            str(x)[0:5] + "0" + str(x)[5:] if len(x) == 6 else str(x) for x in col_date
+        ]
 
         logger.info("Start melting")
-        #Create the new dataframe with unpivoted date columns
+        # Create the new dataframe with unpivoted date columns
         new_df = pd.melt(
             df,
             id_vars=other_col,
             value_vars=col_date,
-            var_name="WeekOfYear",
+            var_name="YearMonth",
             value_name="Value",
         )
 
     logger.info("Start writing in csv")
-    #Write the unpivoted dataframe in csv in the same directory as the initial file
-    new_df.to_csv(args.file[:-5] + "_pivoted.csv", index=False)
+    # Write the unpivoted dataframe in csv in the same directory as the initial file
 
+    new_df.to_csv(args.file.split('.')[0] + "_pivoted.csv", index=False)
 
 if __name__ == "__main__":
     set_log_level(logger, "info")
